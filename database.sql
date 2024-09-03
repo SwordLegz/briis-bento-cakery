c7417c2e9d414c9ea4b4c1c2ac0a9628c9e0455d
Brii SwordLegz <brii.swordlegz@gmail.com>
2024-09-03 16:30:43 -0500

    added stuff to the store reducers, database, and adjusted database name in pool.js

--- database.sql
+++ database.sql
@@ -2,8 +2,49 @@
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
+
+-- CREATE TABLE "user" (
+--     "id" SERIAL PRIMARY KEY,
+--     "username" VARCHAR (80) UNIQUE NOT NULL,
+--     "password" VARCHAR (1000) NOT NULL
+-- );
+
 CREATE TABLE "user" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+	"id" SERIAL PRIMARY KEY,
+	"email" VARCHAR (80) UNIQUE NOT NULL,
+	"password" VARCHAR (1000) NOT NULL,
+	"first_name" VARCHAR (80) NOT NULL,
+	"last_name" VARCHAR (80) NOT NULL
 );
+
+CREATE TABLE "flavors" (
+	"id" SERIAL PRIMARY KEY,
+	"flavor" VARCHAR (80) NOT NULL
+);
+
+CREATE TABLE "orders" (
+	"id" SERIAL PRIMARY KEY,
+	"user_id" INTEGER REFERENCES "user" ON DELETE CASCADE,
+	"date" DATE NOT NULL DEFAULT CURRENT_DATE,
+	"isDone" BOOLEAN NOT NULL
+);
+
+CREATE TABLE "order_items" (
+	"id" SERIAL PRIMARY KEY,
+	"order_id" INTEGER REFERENCES "orders" ON DELETE CASCADE,
+	"flavor_id" INTEGER REFERENCES "flavors" ON DELETE CASCADE,
+	"is_egg_free" BOOLEAN NOT NULL,
+	"is_dairy_free" BOOLEAN NOT NULL,
+	"is_gluten_free" BOOLEAN NOT NULL,
+	"is_vegan" BOOLEAN NOT NULL
+);
+
+INSERT INTO "flavors" 
+	("flavor")
+	VALUES ('Biscoff'),
+	('PBJ'), 
+	('Bacon French Toast'),
+	('Lemon Creme'),
+	('Red Velvet'),
+	('Carrot Cake'),
+	('Cookies & Cream');
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,11 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import logger from 'redux-logger';
 
 import rootReducer from './reducers/_root.reducer'; // imports ./redux/reducers/index.js
 import rootSaga from './sagas/_root.saga'; // imports ./redux/sagas/index.js
 
+// ------------ SAGA MIDDLWARE  ------------ //
 const sagaMiddleware = createSagaMiddleware();
 
 // this line creates an array of all of redux middleware you want to use
@@ -14,10 +15,63 @@ const middlewareList = process.env.NODE_ENV === 'development' ?
   [sagaMiddleware, logger] :
   [sagaMiddleware];
 
+  // ------------ CAKE BITE CART  ------------ //
+  const cart = (state =[], action) => {
+    if (action.type === 'ADD_TO_CART') {
+      let newCart = [...state, aciton.payload]
+      return newCart;
+    } else if (action.type === 'REMOVE_FROM_CART') {
+      let newCart = state.filter((el) => {
+        return el.id !== action.payload
+      })
+      return newCart;
+    }
+    else if (action.type === 'EMPTY_CART') {
+      let cartTotal = action.payload;
+      return cartTotal;
+    }
+    return state;
+  }
+
+  // ------------ CART TOTAL  ------------ //
+  const cartTotal = (state = 0, action) => {
+    if (action.type === 'ADD_CART_TOTAL') {
+      let cartTotal = math.round((state + Number(action.payload)) * 100) / 100;
+      return cartTotal;
+    } else if (action.type === 'REMOVE_CART_TOTAL') {
+      let cartTotal = math.round((state - Number(action.payload)) * 100) / 100;
+      return cartTotal;
+    } else if (action.type === 'CLEAR_CART_TOTAL') {
+      let cartTotal = action.payload;
+      return cartTotal;
+    }
+    return state;
+  }
+
+  // ------------ CUSTOMER INFO  ------------ //
+  const customerInfo = (state = [], action) => {
+    if (action.type === 'ADD_CUSTOMER') {
+      let currentCustomer = [action.payload];
+      return currentCustomer;
+    } else if (action.type === 'CLEAR_CUSTOMER') {
+      let cartTotal = action.payload;
+      return cartTotal;
+    }
+    return state;
+  }
+
+  //  ------------ STORE  ------------ //
 const store = createStore(
+  combineReducers({
   // tells the saga middleware to use the rootReducer
   // rootSaga contains all of our other reducers
-  rootReducer,
+    rootReducer,
+    cart,
+    cartTotal,
+    customerInfo
+  }),
+
+  
   // adds all middleware to our project including saga and logger
   applyMiddleware(...middlewareList),
 );
