eccb419fef1fd6ece45671a325f42a3f3d95afe7
Brii SwordLegz <brii.swordlegz@gmail.com>
2024-09-05 11:36:17 -0500

    put the code in the wrong file, transferred to ShopCakeBites

---
+++ src/components/ShopCakeBites/ShopCakeBites.jsx
@@ -0,0 +1,53 @@
+import { useDispatch, useSelector } from "react-redux";
+import { useState } from "react";
+
+function CakeBiteItem({getFlavor, flavor}) {
+    const [buttonDecision, setButtonDecision] = useState(true)
+    const dispatch = useDispatch();
+
+    let cart = useSelector(store => store.cart)
+    let buttonAction = true;
+    let decidedButton;
+
+        const addToCart = () => {
+            let cartTotalToAdd = flavor.name;
+            let cakeBiteToAdd = {id: flavor.id, flavor: flavor.flavor, quantity: 1}
+            
+            dispatchEvent({
+                type: 'ADD_TO_CART',
+                payload: cakeBiteToAdd
+            })
+            dispatchEvent({
+                type: 'ADD_TO_CART',
+                payload: cakeBiteToAdd
+            })
+            setButtonDecision(false);
+        }
+
+        const removeFromCart = () => {
+            let cakeBiteToRemove = flavor.id
+            let cartTotalToRemove = flavor.name
+            
+            dispatch({
+                type: 'REMOVE_FROM_CART',
+                payload: cakeBiteToRemove
+            })
+
+            dispatch({
+                type: 'REMOVE_CART_TOTAL',
+                payload: cartTotalToRemove
+            })
+            setButtonDecision(true);
+        }
+
+        return (
+            <div id="cakebite-item">
+                <img id="cakebite-img" src={flavor.image} />
+                <h3>{flavor.name}</h3>
+                <p className="flavor-desc">{flavor.description}</p>
+                {buttonDecision ? <button onClick={addToCart}>Add to Cart</button> : <button onClick={removeFromCart}>Remove from Cart</button>}
+            </div>
+        )
+}
+
+export default CakeBiteItem;
